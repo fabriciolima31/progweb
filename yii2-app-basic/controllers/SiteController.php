d3471ef0428ef2e4401de1d47fdb8923c66d1342
Fabricio Lima <fabriciolima31@gmail.com>
2015-07-17 19:47:06 +0000

    testes

--- yii2-app-basic/controllers/SiteController.php
+++ yii2-app-basic/controllers/SiteController.php
@@ -93,4 +93,10 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+    
+    public function actionTeste(){
+        $v = "auhauha";
+        
+        return $this->render('teste',['var'=>"asdasdasd"]);
+    }
 }
